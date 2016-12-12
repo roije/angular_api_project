a64c6ce276c7dca2b3218ca168af28269827b4d2
christianhasselstrom <crellecrelle@gmail.com>
2016-12-12 11:28:28 +0100

    online db added and procfile as well

--- config/config.js
+++ config/config.js
@@ -1,9 +1,6 @@
-/**
- * Created by roije on 12/12/2016.
- */
-
 module.exports = {
-    mongodb: "mongodb://localhost:27017/soccer",
+    //mongodb: "mongodb://localhost:27017/soccer",
+    mongodb: "mongodb://admin:1234@ds127428.mlab.com:27428/angular_api_project",
     players: 'players',
     clubs : 'clubs'
 }
