5aba83f63287bc81280f5c1ff1b50d996030e123
roije <roijellingsgaard@gmail.com>
2016-12-12 10:48:40 +0100

    club routes done

--- routes/club.js
+++ routes/club.js
@@ -6,14 +6,14 @@ var ObjectId = require('mongodb').ObjectId;
 var express = require('express');
 var app = express();
 var url = require('../config/config').mongodb;
-var players = require('../config/config').players;
+var clubs = require('../config/config').clubs;
 
 //Route for creating a club
 app.post('/api/clubs', function(req, res){
 
    MongoClient.connect(url, function(err, db) {
 
-       db.collection(players).insertOne(req.body, function(err, data) {
+       db.collection(clubs).insertOne(req.body, function(err, data) {
            res.json({"msg" : "product created"});
            db.close();
        })
@@ -27,7 +27,7 @@ app.get('/api/clubs', function(req, res) {
 
     MongoClient.connect(url, function(err, db){
 
-        db.collection(players).find({}).toArray(function(err, data) {
+        db.collection(clubs).find({}).toArray(function(err, data) {
             res.json(data);
             db.close();
         })
@@ -39,8 +39,26 @@ app.get('/api/clubs', function(req, res) {
 //Route for getting single club
 app.get('/api/clubs/:id', function(req, res){
     MongoClient.connect(url, function(err, db){
-        db.collection(players).find({ '_id' : ObjectId(req.params.id) }, function(err, data) {
+        db.collection(clubs).findOne({ '_id' : ObjectId(req.params.id) }, function(err, data) {
             res.json(data);
+            db.close();
+        })
+    })
+});
+
+app.delete('/api/clubs/:id', function(req, res) {
+    MongoClient.connect(url, function(err, db){
+        db.collection(clubs).deleteOne({ '_id' : ObjectId(req.params.id) }, function(err, data){
+            res.json({ message : 'Club deleted'});
+            db.close();
+        })
+    })
+});
+
+app.put('/api/clubs/:id', function(req, res) {
+    MongoClient.connect(url, function (err, db) {
+        db.collection(clubs).updateOne({ '_id' : ObjectId(req.params.id)}, {$set : req.body}, function(err, data) {
+            res.json({ 'msg' : 'Club updated'});
             db.close();
         })
     })
